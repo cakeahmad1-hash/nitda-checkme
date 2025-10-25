e7961fe97119533ef6d6958dbeaf5d7aab65bd18
cakeahmad1-hash <cakeahmad1@gmail.com>
2025-10-25 14:42:06 +0100

    Database fixes
    Refactor database initialization to use async/await and create visitors table if it doesn't exist.

--- database.js
+++ database.js
@@ -1,7 +1,27 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+// database.js
+const sqlite3 = require("sqlite3").verbose();
+const { open } = require("sqlite");
 
-const dbPath = path.join(__dirname, 'visitors.db');
-const db = new sqlite3.Database(dbPath);
+async function init() {
+  const db = await open({
+    filename: "./nitda-checkme.db",
+    driver: sqlite3.Database,
+  });
 
-module.exports = db;
+  // Create visitors table if it doesn't exist
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS visitors (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      department TEXT,
+      purpose TEXT,
+      checkin_time TEXT,
+      checkout_time TEXT,
+      status TEXT
+    )
+  `);
+
+  return db;
+}
+
+module.exports = init();
