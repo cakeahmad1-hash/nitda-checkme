b5318d59ddb45f9fe8910a1432b0ac3c7329e057
cakeahmad1-hash <cakeahmad1@gmail.com>
2025-12-08 13:09:17 +0100

    Fix SVG icon rendering in icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -61,3 +61,4 @@ export const EditIcon = ({ className }: { className?: string }) => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.5L15.232 5.232z" />
     </svg>
 );
+trd,bban
